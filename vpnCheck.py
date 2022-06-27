cf1da534ae25864a4f14aa280b1d834e69100d11
Waleed Badar <81442784+walidbadar@users.noreply.github.com>
2022-06-27 20:57:14 +0500

    Update vpnCheck.py

--- vpnCheck.py
+++ vpnCheck.py
@@ -1,3 +1,4 @@
+#!/bin/bash
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -8,7 +9,9 @@ from selenium.webdriver.support import expected_conditions as ec
 import urllib.request, os, subprocess
 
 options = Options()
-# options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(options=options)
 # driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 driver.get('https://ipinfo.io/')
