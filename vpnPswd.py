0846178a5f0c6f118ca8dcb86f0554d2e18f3f87
Waleed Badar <81442784+walidbadar@users.noreply.github.com>
2022-06-27 21:14:35 +0500

    Update vpnPswd.py

--- vpnPswd.py
+++ vpnPswd.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+#!/bin/bash
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -22,7 +22,9 @@ vpnPswd = 'https://raw.githubusercontent.com/walidbadar/freevpn/master/vpnPswd.t
 # open('/etc/openvpn/password.txt', 'wb').write(urllib.request.urlopen(vpnPswd).read())
 
 options = Options()
+options.add_argument('--no-sandbox')
 options.add_argument('--headless')
+options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(options=options)
 driver.get(vpnPswd)
 pswd = WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.XPATH, '/html/body/pre'))).text
@@ -32,7 +34,7 @@ open('/home/'+user+'/openvpn/password.txt', 'w').write(pswd)
 
 vpn = 'https://www.vpnbook.com/free-openvpn-account/VPNBook.com-OpenVPN-US1.zip'
 open('/home/'+user+'/openvpn/openvpn.zip', 'wb').write(requests.get(vpn).content)
-os.system('sudo unzip -o /home/'+user+'/openvpn/openvpn.zip -d /home/'+user+'/openvpn')
+os.system('unzip -o /home/'+user+'/openvpn/openvpn.zip -d /home/'+user+'/openvpn')
 
 vpnSetting = open('/home/'+user+'/openvpn/vpnbook-us1-udp53.ovpn', 'r')
 replacement = ''
